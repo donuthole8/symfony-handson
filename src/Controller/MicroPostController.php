07a7871c1b8cb03f1ee3103fabe9cce17188adde
donuthole8 <muro5866@gmail.com>
2023-05-20 20:12:47 +0900

    feat: フォーム内容をDB保存

--- src/Controller/MicroPostController.php
+++ src/Controller/MicroPostController.php
@@ -4,8 +4,10 @@ namespace App\Controller;
 
 use App\Entity\MicroPost;
 use App\Repository\MicroPostRepository;
+use DateTime;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -35,13 +37,26 @@ class MicroPostController extends AbstractController
     }
 
     #[Route('/micro-post/add', name: 'app_micro_post_add', priority: 2)]
-    public function add(): Response {
+    public function add(Request $request, MicroPostRepository $posts): Response {
         $microPost = new MicroPost();
         $form = $this->createFormBuilder($microPost)
             ->add('title')
             ->add('text')
             ->add('submit', SubmitType::class, ['label' => 'save'])
             ->getForm();
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $post = $form->getData();
+            $post->setCreated(new DateTime());
+            $posts->save($post, true);
+
+            // add a flush
+            // redirect
+        }
+
+        
 
         return $this->renderForm(
             'micro_post/add.html.twig', 
