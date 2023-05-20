731ff1757addd89a8539af4b14cdcf7d211981ee
donuthole8 <muro5866@gmail.com>
2023-05-20 16:45:56 +0900

    feat: 簡単なフォームの作成

--- src/Controller/MicroPostController.php
+++ src/Controller/MicroPostController.php
@@ -4,8 +4,8 @@ namespace App\Controller;
 
 use App\Entity\MicroPost;
 use App\Repository\MicroPostRepository;
-use DateTime;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -32,6 +32,22 @@ class MicroPostController extends AbstractController
         return $this->render('micro_post/show.html.twig', [
             'post' => $post,
         ]);
+    }
 
+    #[Route('/micro-post/add', name: 'app_micro_post_add', priority: 2)]
+    public function add(): Response {
+        $microPost = new MicroPost();
+        $form = $this->createFormBuilder($microPost)
+            ->add('title')
+            ->add('text')
+            ->add('submit', SubmitType::class, ['label' => 'save'])
+            ->getForm();
+
+        return $this->renderForm(
+            'micro_post/add.html.twig', 
+            [
+                'form' => $form
+            ]
+        );
     }
 }
