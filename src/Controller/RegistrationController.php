d841e9a9cf44143530c8d7bc02281d316d9c4ea6
donuthole8 <muro5866@gmail.com>
2023-10-15 21:52:09 +0900

    add:登録フォームのバリデーション

--- src/Controller/RegistrationController.php
+++ src/Controller/RegistrationController.php
@@ -44,7 +44,7 @@ class RegistrationController extends AbstractController
             $entityManager->flush();
 
             // generate a signed url and email it to the user
-            $this->emailVerifier->sendEmailConfirmation('app_verify_email', $user,
+            $this->emailVerifier->sendEmailConfirmation('verify_email', $user,
                 (new TemplatedEmail())
                     ->from(new Address('muro5866@gmail.com', 'MailBot'))
                     ->to($user->getEmail())
@@ -72,12 +72,12 @@ class RegistrationController extends AbstractController
         } catch (VerifyEmailExceptionInterface $exception) {
             $this->addFlash('verify_email_error', $exception->getReason());
 
-            return $this->redirectToRoute('app_register');
+            return $this->redirectToRoute('register');
         }
 
         // @TODO Change the redirect on success and handle or remove the flash message in your templates
         $this->addFlash('success', 'Your email address has been verified.');
 
-        return $this->redirectToRoute('app_register');
+        return $this->redirectToRoute('register');
     }
 }
