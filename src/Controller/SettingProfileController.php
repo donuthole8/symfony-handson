9654268b1ee8ae2e3631992ae5e9e60d953c1af2
donuthole8 <muro5866@gmail.com>
2023-10-21 22:56:23 +0900

    feat:登録フォームにプロフィールフォーム追加

--- src/Controller/SettingProfileController.php
+++ src/Controller/SettingProfileController.php
@@ -5,6 +5,7 @@ namespace App\Controller;
 use App\Entity\User;
 use App\Entity\UserProfile;
 use App\Form\UserProfileFormType;
+use App\Repository\UserRepository;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -12,9 +13,12 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class SettingProfileController extends AbstractController
 {
-    #[Route('/setting/profile', name: 'app_setting_profile')]
-    public function profile(Request $request): Response
-    {
+    #[Route('/setting/profile', name: 'setting_profile')]
+    #[IsGranted('IS_AUTHENTICATED_FULLY')]
+    public function profile(
+        Request $request,
+        UserRepository $userRepository,
+    ): Response {
         /** @var User $user */
         $user = $this->getUser();
         $userProfile = $user->getUserProfile() ?? new UserProfile();
@@ -26,6 +30,16 @@ class SettingProfileController extends AbstractController
 
         if ($form->isSubmitted() && $form->isValid()) {
             $userProfile = $form->getData();
+            $user->setUserProfile($userProfile);
+            $userRepository->save($user, true);
+            $this->addFlash(
+                'success',
+                'Your user profile '
+            );
+            
+            return $this->redirectToRoute(
+                'setting_profile'
+            );
         }
 
         return $this->render('setting_profile/profile.html.twig', [
